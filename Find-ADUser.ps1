39119b92b61a6a6cb3b9b3809dec053d8bf968cc
jonjpbm <jonjpbm@users.noreply.github.com>
2020-04-03 11:58:59 -0500

    Update Find-ADUser.ps1

--- Find-ADUser.ps1
+++ Find-ADUser.ps1
@@ -12,17 +12,15 @@ function Find-ADUser{
     NOTE: When providing both the first and the last name, the last name variable (LN) will not act as a wildcard.
     .Parameter SAM
     The full or partial SAM number or string for a user
-    .Parameter v
-    Pass this switch for a very verbose output of the user(s)
     .INPUTS
     None
     .OUTPUTS
     None
     .NOTES
-    Version:        1.0
+    Version:        2.0
     Author:         Jon Duarte
-    Creation Date:  06132017
-    Purpose/Change: Initial script development
+    Creation Date:  Tuesday, October 8, 2019 11:19:58
+    Purpose/Change: A bunch of changes
 
     .EXAMPLE
     Find-User -FN Jon
@@ -37,102 +35,96 @@ function Find-ADUser{
     .EXAMPLE
     Find-User -SAM 1111234
     .EXAMPLE
-    Find-User.ps1 -FN Jon -LN Duarte -v
-
-
+    Find-User.ps1 -SAMFILE listofsams.txt
+    .EXAMPLE
+    Find-User.ps1 -SAMFILE listofsams.txt -CSV
     #>
     #---------------------------------------------------------[Script Parameters]------------------------------------------------------
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$false)] [string]$FN,
-
-        [Parameter(Mandatory=$false)] [string[]]$LN,
-
-        [Parameter(Mandatory=$false)] [string[]]$SAM,
-
-        [Parameter(Mandatory=$false)] [string]$SAMFile,
-
-        [Parameter(mandatory=$false)] [switch]$RunningLog,
-
-        [Parameter(mandatory=$false)] [switch]$CSV
+        [Parameter(Mandatory=$false)] [string]$FN
+        ,[Parameter(Mandatory=$false)] [string[]]$LN
+        ,[Parameter(Mandatory=$false)] [string[]]$SAM
+        ,[Parameter(Mandatory=$false)] [string]$SAMFile
+        ,[Parameter(mandatory=$false)] [switch]$CSV
+        ,[Parameter(mandatory=$false)] [switch]$RunningLog
+        ,[Parameter(mandatory=$false)] [string]$LogDirectory
+        ,[Parameter(mandatory=$false)] [switch]$NoLog
     )
     #---------------------------------------------------------[Initialisations]--------------------------------------------------------
-
     #Set Error Action to Silently Continue
     $ErrorActionPreference = 'Stop'
 
     #Import Modules & Snap-ins
     Import-Module PoshRSJob
     Import-Module ActiveDirectory
-
     #----------------------------------------------------------[Declarations]----------------------------------------------------------
-
-
-    #Any Global Declarations go here
-    $computerName = $env:COMPUTERNAME
-    $ScriptRoot=$PSScriptRoot
-    $ScriptLogTime = Get-Date -format "yyyyMMddmmss"
-    $ScriptName = (Get-Item $PSCommandPath).Basename
-    $LogDirectory = $ScriptRoot
-    $PSVersionReturned=$PSVersionTable.PSVersion
-    $Date = Get-Date
-
+#Any Global Declarations go here
+$computerName = $env:COMPUTERNAME
+$script = $MyInvocation.MyCommand.Name
+$scriptName = [IO.Path]::GetFileNameWithoutExtension($Script)
+$ScriptLogTime = Get-Date -format "yyyyMMddmmss"
+$PSVersionReturned=$PSVersionTable.PSVersion
+$Date = Get-Date
+if ($NoLog){
+    #Do not create a log file
+    Write-Verbose "No log file switch passed"
+    Write-Verbose "Getting PWD"
+    $PresentWorkingDirectory = Get-Location
+}Else{
+    Write-Verbose "Getting PWD"
+    $PresentWorkingDirectory = Get-Location
+    #Did you pass your own directory for the log?
+    if($PSBoundParameters.ContainsKey('LogDirectory')){
+        Write-Verbose "Log Directory Passed"
+        Write-Verbose $LogDirectory
+    }Else{
+        $LogDirectory = $PresentWorkingDirectory
+    }
+    #Create a unique log file
     if($RunningLog){
-    $ScriptLog= "$LogDirectory\$ScriptName`_$ScriptLogTime.log"
+        Write-Verbose "Running Log switch passed"
+        $ScriptLog= "$LogDirectory\$ScriptName`_$ScriptLogTime.log"
     }else {
-    $ScriptLog= "$LogDirectory\$ScriptName.log"
+        $ScriptLog= "$LogDirectory\$ScriptName.log"
     }
-    #-----------------------------------------------------------[Functions]------------------------------------------------------------
-    function Script_Information {
-        param ()
-        $Date.DateTime
-        Write-Output "Computer Name: $computerName"
-        Write-Output "PowerShell Version $PSVersionReturned"
-        Write-Output "ScriptRoot: $ScriptRoot"
-        Write-Output "ScriptName: $ScriptName"
-        Write-output "ScriptLog: $ScriptLog"
+}
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+function Script_Information {
+    param ()
+    Write-Verbose "Function: $($MyInvocation.MyCommand)"
+    Write-Verbose $Date.DateTime
+    Write-Verbose "Computer Name: $computerName"
+    Write-Verbose "PowerShell Version $PSVersionReturned"
+    Write-Verbose "ScriptName: $ScriptName"
+    Write-Verbose "Present Working Directory: $PresentWorkingDirectory"
+    if($False -eq $NoLog){
+        Write-Verbose "ScriptLog: $ScriptLog"
     }
-    function Test-FileLock {
-        param ([parameter(Mandatory=$true)][string]$Path)
-        $oFile = New-Object System.IO.FileInfo $Path
-        if ((Test-Path -Path $Path) -eq $false){
-            return $false
-        }
-        try{
-            $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
-            if ($oStream){
-            $oStream.Close()
-            }
-            $false
-        }catch{
-            # file is locked by a process.
-            return $true
-        }
-    }
-    Function psjob_cleanup{
-        Param()
-        Write-output "Attempting to clean up the jobs"
-        Get-RSJob | Remove-RSJob
-    }
+}
+Function psjob_cleanup{
+  Param()
+  Write-output "Attempting to clean up the jobs"
+  Get-RSJob | Remove-RSJob
+}
+#-----------------------------------------------------------[Main]------------------------------------------------------------
     Function Main {
         Param ()
         Begin {
-            Write-Output '<description of what is going on>...'
+            Write-Output 'Starting'
             $StopWatch = [System.diagnostics.stopwatch]::StartNew()
             Script_Information
         }
         Process {
             Try {
-                write-output '<code goes here>'
                 #----Initialize Variables----#
-                ##Mail Variables
+                $SearchBase = "DC=usa,DC=ccu,DC=clearchannel,DC=com"
                 $ScriptBlock= {
                     # Parameter help description
                     Param($LoginInScriptBlock)
                     $LoginInScriptBlock
-                    $SearchBase = "DC=usa,DC=ccu,DC=clearchannel,DC=com"
                     Try{
-                        Get-ADUser -Filter "SamAccountName -eq '$LoginInScriptBlock'" -SearchBase $SearchBase -Verbose
+                        Get-ADUser -Filter "SamAccountName -eq '$LoginInScriptBlock'" -SearchBase $Using:SearchBase -Verbose
                     }Catch{
                         Write-error $psitem -Verbose
                     }
@@ -141,9 +133,9 @@ function Find-ADUser{
                 #If-Elses to check what has been passed
                 if ($FN -and $LN)
                 {
-                    Write-Host "It appeats both first and last name were passed"
+                    Write-Host "It appears both first and last name were passed"
                     $filter = "GivenName -like '$FN*' -And Surname -like '$LN*'"
-                    $UserInfo = Get-ADUser -Filter $filter
+                    $UserInfo = Get-ADUser -Filter $filter -SearchBase $SearchBase
                     $UserInfo
                 }
                 elseif ($LN)
@@ -152,7 +144,7 @@ function Find-ADUser{
                     ForEach ($lastname in $LN)
                     {
                         write-host "$lastname"
-                        $UserInfo = Get-ADUser -Filter "Surname -like '$lastname*'"
+                        $UserInfo = Get-ADUser -Filter "Surname -like '$lastname*'" -SearchBase $SearchBase
                         $UserInfo
                     }
                 }
@@ -162,7 +154,7 @@ function Find-ADUser{
                     ForEach ($firstname in $FN)
                     {
                         write-host "$firstname"
-                        $UserInfo = Get-ADUser -Filter "GivenName -like '$firstname*'"
+                        $UserInfo = Get-ADUser -Filter "GivenName -like '$firstname*'" -SearchBase $SearchBase
                         $UserInfo
                     }
                 }
@@ -174,39 +166,41 @@ function Find-ADUser{
                     If($False -eq (Test-Path -Path $SAMFile))
                     {
                         Write-Error "Files does not exist" -ErrorAction Stop
-                    } elseif (condition) {
-
                     }
 
 
                     ##Check if files were passed, use files
                     if( $False -eq $([string]::IsNullOrWhitespace($SAMFile)) ){
-                        Write-Output "Database File passed: $SAMFile"
+                        Write-Output "SAm File passed: $SAMFile"
                         $SAMFile_count=$(Get-Content $SAMFile | Measure-Object -line).Lines
                         Write-Verbose "Lines in passed file: $SAMFile_count"
                         #Use parallel threads
+                        Write-Verbose "Attemtempting to start parallel job threads"
                         Try{
-                            Write-Verbose "Attemtempting to start parallel job threads"
-                            Get-Content $SAMFile | start-rsjob -ScriptBlock $ScriptBlock -ModulesToImport ActiveDirectory -Throttle 10 -ArgumentList $Login
+                            Get-Content $SAMFile | start-rsjob -ScriptBlock $ScriptBlock -ModulesToImport ActiveDirectory -ArgumentList $Login -ErrorAction Stop | Out-Null
                         }Catch{
                             psjob_cleanup
                             Write-error $psitem -Verbose
                         }
 
                         Write-output "Waiting for jobs to finish"
-                        Get-rsjob | wait-rsjob -ShowProgress
+                        Get-rsjob | wait-rsjob -ShowProgress | Out-Null
 
                         $FailedJobs=Get-RSjob | Where-Object {($_.State -eq 'Failed') -or ($_.HasErrors -eq $true)}
 
                         if($FailedJobs){
                             Write-Output "Failed Jobs:"
+                            $FailedJobs | Format-Table -AutoSize
+                            Write-Output "Failed Job Output:"
                             $FailedJobs | Get-rsjob | Receive-rsjob
                             Write-Output "Successfull Jobs:"
-                            $UserInfo= Get-rsjob | Receive-rsjob | Where-Object -Property state -NE 'Failed'
+                            $AllObjects= Get-rsjob | Where-Object {$_.State -eq 'Completed' -and ($_.HasErrors -eq $False)} | Receive-rsjob
                         }Else{
                             Write-output "No Failed jobs were detected"
-                            $UserInfo= Get-rsjob | Receive-rsjob
+                            $AllObjects= Get-rsjob | Receive-rsjob
                         }
+
+                        $AllObjects
 
                         If($False -eq $CSV){
                             $UserInfo
@@ -235,6 +229,7 @@ function Find-ADUser{
                     }
                 }
             }Catch{
+                psjob_cleanup
                 Write-error $PSItem
             }
         }
@@ -247,9 +242,9 @@ function Find-ADUser{
         }
     }
     #-----------------------------------------------------------[Execution]------------------------------------------------------------
-    #Call main
-    if(Test-FileLock -Path $ScriptLog){
-        $ScriptLog= "$LogDirectory\$ScriptName`_$ScriptLogTime.log"
+    if($NoLog){
+        Main *>&1
+    }Else{
+        Main *>&1 | Tee-Object $ScriptLog
     }
-    Main *>&1 | Tee-Object $ScriptLog
 }
